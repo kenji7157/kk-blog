599815df01836e403e5bd9dc2fbbfdb79c3b3c6c
kenji7157 <kawake.kk@gmail.com>
2021-01-09 03:28:06 +0900

    記事一覧の作成

--- packages/apps/pages/index.vue
+++ packages/apps/pages/index.vue
@@ -1,44 +1,49 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <div v-for="(content, index) in contents" :key="index" class="origin">
-            <hr />
-            作成日：{{ content.createdAt }} <br />
-            タイトル：{{ content.title }} <br />
-            本文：{{ content.body }} <br />
-            カテゴリ：{{ content.category.join('/') }}
+  <v-container>
+    <v-row>
+      <v-col class="mt-n4 ml-n2"> トップページ </v-col>
+    </v-row>
+    <v-row class="mt-1">
+      <v-col
+        v-for="(content, index) in contents"
+        :key="index"
+        class="pa-1"
+        cols="4"
+      >
+        <v-card height="300" class="d-flex flex-column">
+          <v-img
+            max-height="180"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          >
+            <div class="text-h6">{{ content.category.join('/') }}</div>
+          </v-img>
+          <v-card-title> {{ content.title }} </v-card-title>
+          <v-card-text class="mt-n3 pb-0">
+            <div>{{ content.body.slice(0, 80) }}...</div>
+          </v-card-text>
+          <div class="mt-auto ml-4 mb-2 text-subtitle-2">
+            {{ content.createdAt }}
           </div>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="mt-3">
+      <div class="text-center">
+        <v-pagination v-model="page" :length="20" circle></v-pagination>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Logo from '~/components/Logo.vue'
-import { articleModule } from '~/store'
+import { articleModule } from '@/store'
 
-@Component({
-  components: {
-    Logo,
-  },
-})
+@Component
 export default class PagesIndex extends Vue {
+  page: 1
   asyncData() {
-    const contents = articleModule.articles
+    const contents = articleModule.articles.slice(0, 6)
     return { contents }
   }
 }

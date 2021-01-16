152b008fd6666f55b4175196d34f8a047708d3c3
kenji7157 <kawake.kk@gmail.com>
2021-01-16 21:03:23 +0900

    デフォルトレイアウトの修正

--- packages/apps/layouts/default.vue
+++ packages/apps/layouts/default.vue
@@ -1,21 +1,9 @@
 <template>
   <v-app dark>
     <v-app-bar :clipped-right="clipped" fixed app>
-      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn> -->
-      <!-- <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn> -->
-      <!-- <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn> -->
       <v-spacer></v-spacer>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn> -->
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     </v-app-bar>
     <v-navigation-drawer
@@ -93,20 +81,6 @@
             <!-- TODO:検索機能は後半に実施する -->
           </v-list-item-content>
         </v-list-item>
-        <!-- <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item> -->
       </v-list>
     </v-navigation-drawer>
     <v-main>
@@ -115,16 +89,6 @@
         <nuxt :nuxt-child-key="$route.fullPath" />
       </v-container>
     </v-main>
-    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer> -->
     <v-footer fixed app>
       <span>NAGANO ENGINEER LIFE © kenji kawanobe 2021</span>
     </v-footer>
@@ -139,9 +103,6 @@ import { articleModule } from '@/store'
 export default class DefaultLayout extends Vue {
   // NOTE: layout配下のコンポーネントはasyncDataメソッドが未定義
   // asyncData()
-
-  created() {}
-
   get archiveArticleList() {
     return articleModule.getArchiveArticleList
   }
@@ -153,22 +114,8 @@ export default class DefaultLayout extends Vue {
   clipped = true
   drawer = false
   fixed = false
-  // items = [
-  //   {
-  //     icon: 'mdi-apps',
-  //     title: 'Welcome',
-  //     to: '/',
-  //   },
-  //   {
-  //     icon: 'mdi-chart-bubble',
-  //     title: 'Inspire',
-  //     to: '/inspire',
-  //   },
-  // ]
-
   miniVariant = false
   right = true
-  // rightDrawer: false,
   title = 'エンジニア覚書 🦒 '
 }
 </script>

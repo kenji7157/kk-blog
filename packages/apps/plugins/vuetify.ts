3c7ae57c0988f2be7ccffcb9aebf1a0f375109a0
kenji7157 <kawake.kk@gmail.com>
2021-01-30 18:59:36 +0900

    vuetifyの軽量化

--- packages/apps/plugins/vuetify.ts
+++ packages/apps/plugins/vuetify.ts
@@ -1,7 +1,81 @@
-import { Framework } from 'vuetify'
+import Vue from 'vue'
 
-declare module 'vue/types/vue' {
-  interface Vue {
-    $vuetify: Framework
-  }
+import Vuetify, {
+  VApp,
+  VAppBar,
+  VBtn,
+  VCard,
+  VCardSubtitle,
+  VCardText,
+  VCardTitle,
+  VChip,
+  VCol,
+  VContainer,
+  VDivider,
+  VFooter,
+  VIcon,
+  VImg,
+  VLazy,
+  VList,
+  VListGroup,
+  VListItem,
+  VListItemContent,
+  VListItemTitle,
+  VMain,
+  VNavigationDrawer,
+  VRow,
+  VTextField,
+  VToolbarTitle,
+} from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VAppBar,
+    VBtn,
+    VCard,
+    VCardSubtitle,
+    VCardText,
+    VCardTitle,
+    VChip,
+    VCol,
+    VContainer,
+    VDivider,
+    VFooter,
+    VIcon,
+    VImg,
+    VLazy,
+    VList,
+    VListGroup,
+    VListItem,
+    VListItemContent,
+    VListItemTitle,
+    VMain,
+    VNavigationDrawer,
+    VRow,
+    VTextField,
+    VToolbarTitle,
+  },
+})
+
+const lightThemeSettings = {
+  primary: '#000000',
+  accent: '#FFFFFF',
+  secondary: '#F8F8FD',
+  info: '#000000',
+  warning: '#000000',
+  error: '#000000',
+  success: '#000000',
 }
+
+export default new Vuetify({
+  theme: {
+    dark: false,
+    themes: {
+      light: lightThemeSettings,
+    },
+  },
+  icons: {
+    iconfont: 'mdi',
+  },
+})

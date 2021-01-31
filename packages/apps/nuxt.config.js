406d22efeb387f7805116a8fe9167cbeed410310
kenji7157 <kawake.kk@gmail.com>
2021-01-31 21:49:19 +0900

    ハイライトcssの修正

--- packages/apps/nuxt.config.js
+++ packages/apps/nuxt.config.js
@@ -63,7 +63,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '~/node_modules/highlight.js/styles/a11y-dark.css',
+    '~/node_modules/highlight.js/styles/atom-one-dark.css',
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
